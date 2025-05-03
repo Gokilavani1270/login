f1a53e9a4b1f4d7097263b185a00baff3fc04dd8
Gokilavani1270 <81812056+Gokilavani1270@users.noreply.github.com>
2025-05-03 10:39:24 +0000

    done

---
+++ mysite/users/views.py
@@ -0,0 +1,66 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
+from .decorators import unauthenticated_user, allowed_users, admin_only
+from django.contrib.auth.models import Group
+
+@unauthenticated_user
+def registerPage(request):
+    form=CreateUserForm()
+
+    if request.method=='POST':
+        form=CreateUserForm(request.POST)
+        if form.is_valid():
+            user=form.save()
+            username=form.cleaned_data.get('username')
+
+            group=Group.objects.get(name='customer')
+            user.groups.add(group)
+            messages.success(request,'Account was created for '+username)
+            return redirect('login')
+
+    context={'form':form}
+    return render(request,'users/register.html',context)
+
+@unauthenticated_user
+def loginPage(request):
+        
+    if request.method=="POST":
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+
+        user=authenticate(request,username=username,password=password)
+
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else:
+            messages.info(request,'username OR password is incorrect')
+
+    context={}
+    return render(request,'users/login.html',context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
+@login_required(login_url='login')
+@admin_only
+def home(request):
+    context={}
+    return render(request,'users/home.html',context)
+
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['admin'])
+def order(request):
+    context={}
+    return render(request,'users/order.html',context)
+
+@login_required(login_url='login')
+def product(request):
+    context={}
+    return render(request,'users/product.html',context)
